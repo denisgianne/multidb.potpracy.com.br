a194e92d944ada23ba6a2aa3ff616647eee246db
Denis Gianne <denis.gianne@gmail.com>
2025-04-10 10:38:35 -0300

    Refatora consulta SQL para listar registros de pessoas, alterando a ordem das colunas para que a coluna 'origem' apareça primeiro, mantendo a identificação das fontes de dados de 15 bancos de dados PostgreSQL.

--- database/example-scripts/fdw/list-all-with-origem.sql
+++ database/example-scripts/fdw/list-all-with-origem.sql
@@ -1,38 +1,38 @@
 -- select count(*) as total
 -- FROM (
-    select *, 'local' as origem from pessoas
+    select 'local' as origem, * from pessoas
         UNION ALL
-    select *, 'external' as origem from external_public.pessoas
+    select 'external' as origem, * from external_public.pessoas
       UNION ALL
-     select *, 'db01' as origem from db01_public.pessoas
+     select 'db01' as origem, * from db01_public.pessoas
       UNION ALL
-    select *, 'db02' as origem from db02_public.pessoas
+    select 'db02' as origem, * from db02_public.pessoas
       UNION ALL
-    select *, 'db03' as origem from db03_public.pessoas
+    select 'db03' as origem, * from db03_public.pessoas
       UNION ALL
-    select *, 'db04' as origem from db04_public.pessoas
+    select 'db04' as origem, * from db04_public.pessoas
       UNION ALL
-    select *, 'db05' as origem from db05_public.pessoas
+    select 'db05' as origem, * from db05_public.pessoas
       UNION ALL
-    select *, 'db06' as origem from db06_public.pessoas
+    select 'db06' as origem, * from db06_public.pessoas
       UNION ALL
-    select *, 'db07' as origem from db07_public.pessoas
+    select 'db07' as origem, * from db07_public.pessoas
       UNION ALL
-    select *, 'db08' as origem from db08_public.pessoas
+    select 'db08' as origem, * from db08_public.pessoas
       UNION ALL
-    select *, 'db09' as origem from db09_public.pessoas
+    select 'db09' as origem, * from db09_public.pessoas
       UNION ALL
-    select *, 'db10' as origem from db10_public.pessoas
+    select 'db10' as origem, * from db10_public.pessoas
       UNION ALL
-    select *, 'db11' as origem from db11_public.pessoas
+    select 'db11' as origem, * from db11_public.pessoas
       UNION ALL
-    select *, 'db12' as origem from db12_public.pessoas
+    select 'db12' as origem, * from db12_public.pessoas
       UNION ALL
-    select *, 'db13' as origem from db13_public.pessoas
+    select 'db13' as origem, * from db13_public.pessoas
       UNION ALL
-    select *, 'db14' as origem from db14_public.pessoas
+    select 'db14' as origem, * from db14_public.pessoas
       UNION ALL
-    select *, 'db15' as origem from db15_public.pessoas
+    select 'db15' as origem, * from db15_public.pessoas
 
     ORDER BY nome
 -- ) as total;
