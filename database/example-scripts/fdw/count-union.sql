5a58b52626ba55acf2559be6302d9605dccfc759
Denis Gianne <denis.gianne@gmail.com>
2025-04-10 01:15:54 -0300

    Refatora script SQL para contagem de registros, simplificando a consulta ao remover redundâncias e mantendo a inclusão de 15 bancos de dados PostgreSQL.

--- database/example-scripts/fdw/count-union.sql
+++ database/example-scripts/fdw/count-union.sql
@@ -1,84 +1,34 @@
-select
-    count(*) as total
-FROM
-    (
-        select
-            *
-        from
-            external_public.pessoas
-        UNION ALL
-        select
-            *
-        from
-            db01_public.pessoas
-        UNION ALL
-        select
-            *
-        from
-            db02_public.pessoas
-        UNION ALL
-        select
-            *
-        from
-            db03_public.pessoas
-        UNION ALL
-        select
-            *
-        from
-            db04_public.pessoas
-        UNION ALL
-        select
-            *
-        from
-            db05_public.pessoas
-        UNION ALL
-        select
-            *
-        from
-            db06_public.pessoas
-        UNION ALL
-        select
-            *
-        from
-            db07_public.pessoas
-        UNION ALL
-        select
-            *
-        from
-            db08_public.pessoas
-        UNION ALL
-        select
-            *
-        from
-            db09_public.pessoas
-        UNION ALL
-        select
-            *
-        from
-            db10_public.pessoas
-        UNION ALL
-        select
-            *
-        from
-            db11_public.pessoas
-        UNION ALL
-        select
-            *
-        from
-            db12_public.pessoas
-        UNION ALL
-        select
-            *
-        from
-            db13_public.pessoas
-        UNION ALL
-        select
-            *
-        from
-            db14_public.pessoas
-        UNION ALL
-        select
-            *
-        from
-            db15_public.pessoas
-    ) as total;
+select count(*) as total
+FROM (
+    select * from external_public.pessoas
+      UNION ALL
+     select * from db01_public.pessoas
+      UNION ALL
+    select * from db02_public.pessoas
+      UNION ALL
+    select * from db03_public.pessoas
+      UNION ALL
+    select * from db04_public.pessoas
+      UNION ALL
+    select * from db05_public.pessoas
+      UNION ALL
+    select * from db06_public.pessoas
+      UNION ALL
+    select * from db07_public.pessoas
+      UNION ALL
+    select * from db08_public.pessoas
+      UNION ALL
+    select * from db09_public.pessoas
+      UNION ALL
+    select * from db10_public.pessoas
+      UNION ALL
+    select * from db11_public.pessoas
+      UNION ALL
+    select * from db12_public.pessoas
+      UNION ALL
+    select * from db13_public.pessoas
+      UNION ALL
+    select * from db14_public.pessoas
+      UNION ALL
+    select * from db15_public.pessoas
+) as total;
